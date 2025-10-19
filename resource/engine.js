d7bf1bb51535881a7d12abd4a5a6c552d95c807e
Manoelrev <jmanoel890@gmail.com>
2025-10-19 08:46:12 -0300

    criação da render 3d

--- resource/engine.js
+++ resource/engine.js
@@ -5,7 +5,80 @@ const ctx = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 600;
 
-// Variáveis do jogo
+
+// Variavéís
+scale = 100
+angle = 0
+circle_pos = [canvas.width/2, canvas.height/2]
+
+function mdotmatrix(matrix1, matrix2) {
+    result = []
+    n = 0
+    matrix1.forEach(mat => {
+        result[n] = mat[0] * matrix2[0] + mat[1] * matrix2[1] + mat[2] * matrix2[2]
+        n++
+    });
+    return result
+}
+
+cube = [[-1, -1,  1],
+        [ 1, -1,  1],
+        [ 1,  1,  1],
+        [-1,  1,  1],
+        [-1, -1, -1],
+        [ 1, -1, -1],
+        [ 1,  1, -1],
+        [-1,  1, -1]]
+
+projection_matrix = [[1, 0, 0],
+                     [0, 1, 0]]
+
+projected_points = []
+
+function render3d() {
+    rotation_z = [
+        [Math.cos(angle), -Math.sin(angle), 0],
+        [Math.sin(angle), Math.cos(angle), 0],
+        [0, 0, 1]]
+
+    rotation_y = [
+        [Math.cos(angle), 0, Math.sin(angle)],
+        [0, 1, 0],
+        [-Math.sin(angle), 0, Math.cos(angle)]]
+
+    rotation_x = [
+        [1, 0, 0],
+        [0, Math.cos(angle), -Math.sin(angle)],
+        [0, Math.sin(angle), Math.cos(angle)]]
+
+    angle += 0.01
+
+    i = 0
+    cube.forEach(point => {
+        rotated2d = mdotmatrix(rotation_z, point)
+        
+        rotated2d = mdotmatrix(rotation_y, rotated2d)
+        rotated2d = mdotmatrix(rotation_x, rotated2d)
+
+        projected2d = mdotmatrix(projection_matrix, rotated2d)
+        
+        x = projected2d[0] * scale + circle_pos[0]
+        y = projected2d[1] * scale + circle_pos[1]
+
+        projected_points[i] = [x, y]
+        i += 1
+    });
+    projected_points.forEach(x => {
+    projected_points.forEach(point => {
+        ctx.beginPath();
+        ctx.moveTo(x[0], x[1]);
+        ctx.lineTo(point[0], point[1]);
+        // Draw the Path
+        ctx.stroke();
+    })});
+}
+                     
+// Variáveis da engine
 let lastTime = 0;
 let accumulatedTime = 0;
 let timeStep = 1000/60; // 60 FPS (em milissegundos)
@@ -19,7 +92,7 @@ function render() {
 
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+    render3d()
 }
 
 function gameLoop(timestamp) {
@@ -38,4 +111,3 @@ function gameLoop(timestamp) {
 }
 
 requestAnimationFrame(gameLoop);
-
